e8fc257cd17f33fb19f8ee58b7e742efe9d379c1
maczin13f <154028448+maczin13f@users.noreply.github.com>
2025-07-26 22:56:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,154 @@
 const data = new Date();
+
 let quantia = JSON.parse(localStorage.getItem('quantidade'));
 
 
+
+
+
 function obterDiaAtual() {
+
   const diaAtual = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(data);
+
   console.log('Hoje é:', diaAtual);
+
 }
+
 obterDiaAtual();
 
+
+
 function atualizarContadorCarrinho() {
+
   const contador = document.getElementById('contador');
+
   const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
   if (contador) {
+
     contador.textContent = carrinho.length;
+
   }
+
 }
+
 atualizarContadorCarrinho();
 
+
+
 // Captura todos os pratos do cardápio
+
 const pratos = document.querySelectorAll('.cardapio');
+
 const secaoCardapio = document.getElementById('pratos');
+
 const secaoInfo = document.querySelector('#pratosinfo');
+
 const pratosInfo = document.querySelectorAll('.infoprato'); // use class nas divs dentro de #pratosinfo
 
+
+
 pratos.forEach(pratoDiv => {
+
   const nome = pratoDiv.querySelector('h2').textContent;
+
   const descricao = pratoDiv.querySelector('p').textContent;
+
   const preco = pratoDiv.querySelector('h3').textContent.replace('Preço: ', '');
+
   const imagem = pratoDiv.querySelector('img').getAttribute('src');
 
+
+
   // Botão adicionar ao carrinho
+
   const botao = document.createElement('button');
+
   botao.textContent = 'Adicionar ao carrinho';
+
   botao.onclick = function () {
+
     
+
     const quantidadeSelecionada = Number(inputQuantidade.value);
 
+
+
  inputQuantidade.value = 1;
+
     
+
     const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
     const jaExiste = carrinho.some(item => item.nome === nome && item.descricao === descricao);
+
     if (jaExiste) {
+
       alert(`O prato "${nome}" já foi adicionado ao carrinho.`);
+
       return;
+
       
+
     }
+
     adicionarNoCarrinho(nome, descricao, preco, imagem, quantidadeSelecionada);
+
     atualizarContadorCarrinho();
+
   };
 
+
+
   // Botão quantidade
+
   const btnQuantidade = document.createElement('button');
+
   btnQuantidade.className = 'btnQuantidadeAdd';
+
   btnQuantidade.textContent = '➕';
 
+
+
   const inputQuantidade = document.createElement('input');
+
   inputQuantidade.type = 'number';
+
   inputQuantidade.value = 1;
+
   inputQuantidade.min = 1;
+
   inputQuantidade.max = 6;
 
+
+
   btnQuantidade.addEventListener('click', function () {
+
     let valorAtual = Number(inputQuantidade.value);
+
     if (valorAtual < 6) {
+
       inputQuantidade.value = valorAtual + 1;
+
     }
+
   });
 
+
+
   pratoDiv.appendChild(botao);
+
   pratoDiv.appendChild(inputQuantidade);
+
   pratoDiv.appendChild(btnQuantidade);
+
   
+
   const imgCardapio = pratoDiv.querySelector('.cardapio img')
+
+
+
+
+
 });
+
